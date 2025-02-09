a8c8bceb1550c7375943a7b5f06ab0e3ba3ca20a
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2025-02-09 12:14:19 -0600

    the og bulletarm code is outdated fixing it so it runs properly

--- tutorials/block_stacking_demo.py
+++ tutorials/block_stacking_demo.py
@@ -1,15 +1,27 @@
 from bulletarm import env_factory
 
 def runDemo():
-  env_config = {'render': True}
-  env = env_factory.createEnvs(1, 'block_stacking', env_config)
+    env_config = {
+        'render': True,
+        'physics_mode': 'direct',
+        'render_mode': 'gui'
+    }
 
-  obs = env.reset()
-  done = False
-  while not done:
-    action = env.getNextAction()
-    obs, reward, done = env.step(action)
-  env.close()
+    env = None
+    try:
+        env = env_factory.createSingleProcessEnv(env_type='block_stacking', env_config=env_config)
 
-if __name__ == '__main__':
-  runDemo()
+        obs = env.reset()
+        done = False
+        while not done:
+            action = env.getNextAction()
+            obs, reward, done, _ = env.step(action)
+
+    except Exception as e:
+        print(f"Execution error: {str(e)}")
+    finally:
+        if env is not None:
+            env.close()
+
+if __name__ == "__main__":
+    runDemo()
